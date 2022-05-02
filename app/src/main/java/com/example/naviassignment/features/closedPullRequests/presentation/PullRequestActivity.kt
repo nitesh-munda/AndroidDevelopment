83c91590ccfca6af60324f44a05736787524fedc
Nitesh_Munda <niteshsingh.m@dunzo.in>
2022-05-02 10:03:31 +0530

    project init commit

---
+++ app/src/main/java/com/example/naviassignment/features/closedPullRequests/presentation/PullRequestActivity.kt
@@ -0,0 +1,7 @@
+package com.example.naviassignment.features.closedPullRequests.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+
+class PullRequestActivity: AppCompatActivity() {
+
+}
